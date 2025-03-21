94b5d00e7716abd3d4a153a66779d78914db5f45
Rodrigo Feitosa <rodrigofeitosarodrigues@gmail.com>
2025-03-20 22:00:01 -0300

    Adicionando Configurações de Routas

---
+++ gatewayserver/src/main/java/com/ms/rr/gatewayserver/routes/Routes.java
@@ -0,0 +1,32 @@
+package com.ms.rr.gatewayserver.routes;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+
+@Configuration(proxyBeanMethods = false)
+public class Routes {
+
+    @Bean
+    public RouteLocator appRouterConfig(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/eazybank/accounts/**")
+                        .filters( f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://ACCOUNTS"))
+                .route(p -> p
+                        .path("/eazybank/loans/**")
+                        .filters( f -> f.rewritePath("/eazybank/loans/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://LOANS"))
+                .route(p -> p
+                        .path("/eazybank/cards/**")
+                        .filters( f -> f.rewritePath("/eazybank/cards/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://CARDS")).build();
+    }
+}
