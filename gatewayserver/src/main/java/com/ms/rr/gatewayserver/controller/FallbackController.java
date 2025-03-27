215ddfb9fc12ac86c22a1c7776a12f7785c60b83
Rodrigo Feitosa <rodrigofeitosarodrigues@gmail.com>
2025-03-26 22:47:14 -0300

    Implementando Circuit Breaker Pattern no Gateway

---
+++ gatewayserver/src/main/java/com/ms/rr/gatewayserver/controller/FallbackController.java
@@ -0,0 +1,14 @@
+package com.ms.rr.gatewayserver.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+
+    @RequestMapping("/contactSupport")
+    public Mono<String> contactSupport() {
+        return Mono.just("A error occurred. Please contact the support team.");
+    }
+}
--- gatewayserver/src/main/java/com/ms/rr/gatewayserver/routes/Routes.java
+++ gatewayserver/src/main/java/com/ms/rr/gatewayserver/routes/Routes.java
@@ -16,17 +16,23 @@ public class Routes {
                 .route(p -> p
                         .path("/eazybank/accounts/**")
                         .filters( f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)","/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("accountsCircuitBreaker")
+                                        .setFallbackUri("forward:/contactSupport")))
                         .uri("lb://ACCOUNTS"))
                 .route(p -> p
                         .path("/eazybank/loans/**")
                         .filters( f -> f.rewritePath("/eazybank/loans/(?<segment>.*)","/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("loansCircuitBreaker")
+                                        .setFallbackUri("forward:/contactSupport")))
                         .uri("lb://LOANS"))
                 .route(p -> p
                         .path("/eazybank/cards/**")
                         .filters( f -> f.rewritePath("/eazybank/cards/(?<segment>.*)","/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("cardsCircuitBreaker")
+                                        .setFallbackUri("forward:/contactSupport")))
                         .uri("lb://CARDS")).build();
     }
 }
