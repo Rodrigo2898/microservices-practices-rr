f5c55412ffdf332c6fc4d9cb6f4f8c983297f735
Rodrigo Feitosa <rodrigofeitosarodrigues@gmail.com>
2025-04-04 23:58:59 -0300

    Implementando Redis RateLimiter no Gateway

--- gatewayserver/src/main/java/com/ms/rr/gatewayserver/configs/GatewayConfigs.java
+++ gatewayserver/src/main/java/com/ms/rr/gatewayserver/configs/GatewayConfigs.java
@@ -1,21 +1,24 @@
-package com.ms.rr.gatewayserver.routes;
+package com.ms.rr.gatewayserver.configs;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-@Configuration(proxyBeanMethods = false)
-public class Routes {
+@Configuration
+public class GatewayConfigs {
 
     @Bean
     public RouteLocator appRouterConfig(RouteLocatorBuilder builder) {
@@ -42,7 +45,9 @@ public class Routes {
                         .filters( f -> f.rewritePath("/eazybank/cards/(?<segment>.*)","/${segment}")
                                 .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
                                 .circuitBreaker(config -> config.setName("cardsCircuitBreaker")
-                                        .setFallbackUri("forward:/contactSupport")))
+                                        .setFallbackUri("forward:/contactSupport"))
+                                .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+                                        .setKeyResolver(userKeyResolver())))
                         .uri("lb://CARDS")).build();
     }
 
@@ -55,5 +60,19 @@ public class Routes {
                         .timeoutDuration(Duration.ofSeconds(4))
                         .build())
                 .build());
+    }
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter() {
+        return new RedisRateLimiter(1,1,1);
+    }
+
+    @Bean
+    public KeyResolver userKeyResolver() {
+        return exchange -> Mono.justOrEmpty(exchange
+                .getRequest()
+                .getHeaders()
+                .getFirst("user"))
+                .defaultIfEmpty("anonymous");
     }
 }
